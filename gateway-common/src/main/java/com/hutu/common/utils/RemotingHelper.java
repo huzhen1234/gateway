51c5823bdf2e04e0aff68837604f63ffcfddfe1c
huzhen <2027280373@qq.com>
2025-08-10 22:06:13 +0800

    相关工具类

---
+++ gateway-common/src/main/java/com/hutu/common/utils/RemotingHelper.java
@@ -0,0 +1,59 @@
+package com.hutu.common.utils;
+
+import io.netty.channel.Channel;
+
+import java.net.SocketAddress;
+
+public class RemotingHelper {
+	
+    public static String exceptionSimpleDesc(final Throwable e) {
+        StringBuilder sb = new StringBuilder();
+        if (e != null) {
+            sb.append(e.toString());
+
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            if (stackTrace != null && stackTrace.length > 0) {
+                StackTraceElement elment = stackTrace[0];
+                sb.append(", ");
+                sb.append(elment.toString());
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static SocketAddress string2SocketAddress(final String addr) {
+        return RemotingUtil.string2SocketAddress(addr);
+    }
+
+    public static String parseChannelRemoteAddr(final Channel channel) {
+        if (null == channel) {
+            return "";
+        }
+        SocketAddress remote = channel.remoteAddress();
+        final String addr = remote != null ? remote.toString() : "";
+
+        if (!addr.isEmpty()) {
+            int index = addr.lastIndexOf("/");
+            if (index >= 0) {
+                return addr.substring(index + 1);
+            }
+
+            return addr;
+        }
+
+        return "";
+    }
+
+    public static String parseSocketAddressAddr(SocketAddress socketAddress) {
+        if (socketAddress != null) {
+            final String addr = socketAddress.toString();
+
+            if (!addr.isEmpty()) {
+                return addr.substring(1);
+            }
+        }
+        return "";
+    }
+
+}
